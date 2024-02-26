02a5b9b2cce80fe62b043ebaf1cd0de68104be18
Widyarso Joko Purnomo <widyjp10@gmail.com>
2024-02-26 11:55:24 +0700

    export transaction manager

--- lib/near_api_flutter.dart
+++ lib/near_api_flutter.dart
@@ -4,6 +4,7 @@ export 'package:near_api_flutter/src/account.dart';
 export 'package:near_api_flutter/src/contract.dart';
 export 'package:near_api_flutter/src/key_store.dart';
 export 'package:near_api_flutter/src/wallet.dart';
+export 'package:near_api_flutter/src/transaction_api/transaction_manager.dart';
 export 'package:near_api_flutter/src/transaction_api/rpc_providers.dart';
 export 'package:near_api_flutter/src/models/access_key.dart';
 export 'package:near_api_flutter/src/utils.dart';
